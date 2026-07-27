9c6cded540ff62eaa742ac68e52c32f3a60c0265
subhrajit95 <subhrajitshome95@gmail.com>
2026-07-27 21:25:48 +0530

    made changes

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Atomic Habits",
+        "author": "James Clear"
+    },
+    {
+        "id": 2,
+        "title": "Deep Work",
+        "author": "Cal Newport"
+    }
+]
+
 @app.get("/")
 def home():
-    return {"message": "Hello from AWS!"}
+    return {
+        "message": "Welcome to the Books API"
+    }
+
+
+@app.get("/books")
+def get_books():
+    return books
